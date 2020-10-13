41e208a49e87aeefea8265c88bd44eaf0616ac6b
Rodrigo Fonseca <ro.fonseca.melo@gmail.com>
2020-10-13 14:18:13 -0300

    feat(adiciona o fluxo para criar uploads): adiciona o fluxo para criar multiplos uploads

--- src/api/controllers/uploads.js
+++ src/api/controllers/uploads.js
@@ -1,10 +1,25 @@
 const CsvParseService = require('api/services/CsvParseService')
+const error = require('api/services/core/error')
+const Uploads = require('api/models/Uploads')
 
 async function create(req, res, next) {
-  const csvParseService = new CsvParseService(req.file.buffer)
-  const parsed = await csvParseService.parse()
+  try {
+    const csvParseService = new CsvParseService(req.file.buffer)
+    const parsed = await csvParseService.parse()
+    const filtered = filterByNotTitleRows(parsed)
+    const instance = new Uploads()
 
-  console.info('parsed :>> ', parsed)
+    return instance.add(filtered).then(
+      data => res.send({ data }),
+      err => next(err)
+    )
+  } catch (e) {
+    return next(new error.InternalServerError(e))
+  }
+}
+
+function filterByNotTitleRows(rows) {
+  return rows.filter((_row, i) => i !== 0)
 }
 
 module.exports = {
--- src/api/models/Uploads.js
+++ src/api/models/Uploads.js
@@ -1,12 +1,29 @@
 const dbService = require('api/services/core/dbService')
 const sequelize = require('api/services/core/sequelize')
+const error = require('api/services/core/error')
+const _ = require('lodash')
 
 class Uploads {
   constructor() {
     this.model = this._getModel()
   }
 
-  create({ yardCode, employeeCode, clockIn, clockOut, filename }) {
+  add(data) {
+    if (!_.isArray(data)) return Promise.reject(new error.BadRequestError())
+    if (_.isEmpty(data)) return Promise.reject(new error.BadRequestError())
+
+    return sequelize.get().transaction(transaction => {
+      const mapped = this.mapToCreateFormat(data)
+      const promises = mapped.map(data =>
+        this.create(data, 'teste', transaction)
+      )
+
+      return Promise.all(promises)
+    })
+  }
+
+  create({ yardCode, employeeCode, clockIn, clockOut }, filename, transaction) {
+    let method = 'create'
     const { model } = this
     const data = {
       yardCode,
@@ -16,7 +33,21 @@ class Uploads {
       filename,
     }
 
-    return dbService.create({ model, data })
+    if (transaction) method = 'createWithTransaction'
+    return dbService[method]({ model, data, transaction })
+  }
+
+  mapToCreateFormat(data) {
+    return data.map(data => {
+      const [yardCode, employeeCode, clockIn, clockOut] = data
+
+      return {
+        yardCode,
+        employeeCode,
+        clockIn,
+        clockOut,
+      }
+    })
   }
 
   _getModel() {
